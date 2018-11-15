fda310c63e6ad52df5786a818b6e26e095291386
Chris Myrick <ChrisM@svmic.com>
2018-11-15 10:19:35 -0600

    Stub for CompRoot

---
+++ CompRoot_WithDI/Presenters/IPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using CastleWindsorDI_Example.Interfaces;
+using CastleWindsorDI_Example.Views;
+
+namespace CastleWindsorDI_Example.Presenters
+{
+    public interface IPresenter
+    {
+        void Display();
+
+        void Initialize(IMainView view);
+
+        void DoSomething();
+        void RobBank();
+        void Shoot();
+        void Arrest();
+        void Speak();
+    }
+}
---
+++ CompRoot_WithDI/Presenters/Presenter.cs
@@ -0,0 +1,55 @@
+﻿//using CastleWindsorDI_Example.Interfaces;
+//using CastleWindsorDI_Example.Views;
+//using System;
+
+//namespace CastleWindsorDI_Example.Presenters
+//{
+//    public class Presenter : IPresenter
+//    {
+//        private MainView View { get; set; }
+//        private ICriminal Criminal { get; set; }
+//        private IPoliceOfficer PoliceOfficer { get; set; }
+
+//        public Presenter(ICriminal criminal, IPoliceOfficer officer) 
+//        {
+//            Criminal = criminal;
+//            PoliceOfficer = officer;
+//        }
+
+//        public void Display()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Initialize(IMainView view)
+//        {
+//            view.Message = "This is a message from the presenter.";
+//        }
+
+//        public void DoSomething()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void RobBank()
+//        {
+//            Criminal.RobBank();
+//        }
+
+//        public void Shoot()
+//        {
+//            PoliceOfficer.Shoot();
+//        }
+
+//        public void Arrest()
+//        {
+//            PoliceOfficer.Arrest();
+//        }
+
+//        public void Speak()
+//        {
+//            Criminal.Speak();
+//            PoliceOfficer.Speak();
+//        }
+//    }
+//}
